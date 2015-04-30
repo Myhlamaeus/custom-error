8e1d1e6b7d87acbda002b4a79100e169a06f5bd4
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-04-30 16:48:49 +0000

    Use standard code style

--- custom-error.js
+++ custom-error.js
@@ -1,29 +1,29 @@
-const stackSeparator = `\n${" ".repeat(4)}at `;
+const stackSeparator = `\n${' '.repeat(4)}at `
 
 export default class {
-    constructor(name, message, trace) {
-        var stack;
+  constructor (name, message, trace) {
+    let stack
 
-        if(typeof(trace) === "undefined") {
-            const tmp = new Error();
+    if (typeof trace === 'undefined') {
+      const tmp = new Error()
 
-            if(tmp.stack) {
-                stack = tmp.stack;
-            }
-        } else if(Array.isArray(trace)) {
-            stack = trace.map((pos) => `${pos.file}:${pos.line}:${pos.column}`).join(stackSeparator);
-        } else {
-            stack = trace;
-        }
-
-        Object.defineProperties(this, {
-            "name": descVal(name),
-            "message": descVal(message),
-            "stack": descVal(stack)
-        });
+      if (tmp.stack) {
+        stack = tmp.stack
+      }
+    } else if (Array.isArray(trace)) {
+      stack = trace.map((pos) => `${pos.file}:${pos.line}:${pos.column}`).join(stackSeparator)
+    } else {
+      stack = trace
     }
 
-    toString() {
-        return `${this.name}: ${this.message}`;
-    }
+    Object.defineProperties(this, {
+      name: name,
+      message: message,
+      stack: stack
+    })
+  }
+
+  toString () {
+    return `${this.name}: ${this.message}`
+  }
 }
